7ccb483cfc23a51f9b6d7e3548186efc8ef7763b
mizumaki-web <yousei_ft@yahoo.co.jp>
2025-04-25 20:39:15 +0900

    Update TaskManager.java

--- cli-task-manager/src/q5/TaskManager.java
+++ cli-task-manager/src/q5/TaskManager.java
@@ -10,16 +10,20 @@ public class TaskManager {
         tasks.add(new Task(title));
     }
     public void showTasks(){
-        for(Task task: tasks){
-            System.out.println(task.getTitle()+"-"+(task.isDone() ? "完了" : "未完了"));
+        for(int i = 0; i < tasks.size(); i++){
+            Task task = tasks.get(i);
+            String status = task.isDone() ? "完了" : "未完了";
+            System.out.println((i + 1) + ":" + task.getTitle() + "-" + status);
         }
     }
-    public void completeTask(int index){
+    public void completeTask(int userInputIndex){
+        int index = userInputIndex - 1; //添え字を0に戻す
+
         if(index >= 0 && index < tasks.size()){
             tasks.get(index).complete();
             System.out.println("タスク"+tasks.get(index).getTitle()+"を完了しました。");
         }else {
-            System.out.println("無効なタスク番号です");
+            System.out.println("無効なタスク番号です。１から");
         }
     }
 }
