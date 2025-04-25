4d6b4ed64717aa88b1c974127cef6f65aebee0a1
mizumaki-web <yousei_ft@yahoo.co.jp>
2025-04-25 14:49:44 +0900

    Create Main.java

---
+++ cli-task-manager/src/q5/Main.java
@@ -0,0 +1,26 @@
+package q5;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        TaskManager manager = new TaskManager();
+        for(int i = 0; i < args.length ; i++) {
+            Task task = new Task(args[i]);
+            manager.addTask(args[i]);
+        }
+        manager.showTasks();
+
+        while (true) {
+            System.out.print("完了するタスクの番号を入力（-1で終了）: ");
+            int index = scanner.nextInt();
+            if (index == -1) {
+                break;
+            }
+            manager.completeTask(index);
+            manager.showTasks();
+        }
+    }
+}
